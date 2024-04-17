e3e3f7bddfbb23dbe2172bbe4a00f2af440615d8
mithun <mithunchakladar123@gmail.com>
2024-04-17 23:02:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import Navbar from "./Components/Navbar/Navbar";
-import CarPage from "./pages/CarPage/CarPage";
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import Footer from "./Components/Footer/Footer";
@@ -18,7 +17,6 @@ const App = () => {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/Login" element={<LoginPage />} />
         <Route path="/Signup" element={<SignupPage />} />
-        <Route path="/specs" element={<CarPage />} />
 
       </Routes>
       <Footer />
