e00d129d277686fd6df5204a62138f9557464a86
unknown <tusharrawat654@gmail.com>
2024-04-18 10:49:27 +0530

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import './Navbar.css'
  const Navbar = ()=> {
+  const navigate = useNavigate();
     return   <nav className="navbar">
     <div className="logo">Porsche</div>
     <div className="nav-links">
-      <a href="/home" className="nav-item">Home</a>
-      <a href="/login" className="nav-item">Login</a>
-      <a href="/Signup" className="nav-item">Signup</a>
-      <a href='/contact' className='nav-contact'>Contact</a>
+      <div onClick={()=>{navigate('/home')}} className="nav-item">Home</div>
+      <div onClick={()=>{navigate('/login')}}  className="nav-item">Login</div>
+      <div onClick={()=>{navigate('/Signup')}}  className="nav-item">Signup</div>
+      <div  onClick={()=>{navigate('/contact')}} className='nav-contact'>Contact</div>
     </div>
   </nav>
  }
